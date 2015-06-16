af395a74f2401260535bc78fb99b1b5c2571f290
Daniel X Moore <yahivin@gmail.com>
2015-06-16 12:57:21 -0700

    Basic hyperweb skeleton app

--- index.js
+++ index.js
@@ -4,10 +4,16 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
-
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
+});
+
+app.get('/', function(request, response) {
+  response.render('index');
+});
+
+app.get('/yo', function(request, response) {
+  response.json({
+    yo: "holla!"
+  })
 });
